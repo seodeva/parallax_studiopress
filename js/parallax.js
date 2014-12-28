1f7b074b233da279ead7c3cf4abc5ebd0e9dcc96
Valentina Mercenaro <mercenaro.valentina@gmail.com>
2014-12-29 00:08:23 +0100

    Update parallax.js

--- js/parallax.js
+++ js/parallax.js
@@ -1,41 +1,54 @@
-jQuery(function( $ ){
-
-	// Enable parallax and fade effects on homepage sections
-	$(window).scroll(function(){
-
-		scrolltop = $(window).scrollTop()
-		scrollwindow = scrolltop + $(window).height();
-
-		$(".home-section-1").css("backgroundPosition", "50% " + -(scrolltop/6) + "px");
-
-		if ( $(".home-section-3").length ) {
-		
-			sectionthreeoffset = $(".home-section-3").offset().top;		  
-
-			if( scrollwindow > sectionthreeoffset ) {
-
-				// Enable parallax effect
-				backgroundscroll = scrollwindow - sectionthreeoffset;
-				$(".home-section-3").css("backgroundPosition", "50% " + -(backgroundscroll/6) + "px");
-
-			}
-		
-		}
-
-		if ( $(".home-section-5").length ) {
-		
-			sectionfiveoffset = $(".home-section-5").offset().top;		  
-
-			if( scrollwindow > sectionfiveoffset ) {
-
-				// Enable parallax effect
-				backgroundscroll = scrollwindow - sectionfiveoffset;
-				$(".home-section-5").css("backgroundPosition", "50% " + -(backgroundscroll/6) + "px");
-
-			}
-		
-		}
-
-	});
-
+jQuery(function( $ ){
+
+	// Enable parallax and fade effects on homepage sections
+	$(window).scroll(function(){
+
+		scrolltop = $(window).scrollTop()
+		scrollwindow = scrolltop + $(window).height();
+
+		$(".home-section-1").css("backgroundPosition", "0px " + -(scrolltop/6) + "px");
+
+		if( scrollwindow > $(".home-section-3").offset().top ) {
+
+			// Enable parallax effect
+			backgroundscroll = scrollwindow - $(".home-section-3").offset().top;
+			$(".home-section-3").css("backgroundPosition", "0px " + -(backgroundscroll/6) + "px");
+
+		}
+
+		if( scrollwindow > $(".home-section-5").offset().top ) {
+
+			// Enable parallax effect
+			backgroundscroll = scrollwindow - $(".home-section-5").offset().top;
+			$(".home-section-5").css("backgroundPosition", "0px " + -(backgroundscroll/6) + "px");
+
+		}
+		
+		if( scrollwindow > $(".home-section-7").offset().top ) {
+
+			// Enable parallax effect
+			backgroundscroll = scrollwindow - $(".home-section-7").offset().top;
+			$(".home-section-7").css("backgroundPosition", "0px " + -(backgroundscroll/6) + "px");
+
+		}
+
+       if( scrollwindow > $(".home-section-9").offset().top ) {
+
+			// Enable parallax effect
+			backgroundscroll = scrollwindow - $(".home-section-9").offset().top;
+			$(".home-section-9").css("backgroundPosition", "0px " + -(backgroundscroll/6) + "px");
+
+		}
+		
+		if( scrollwindow > $(".home-section-11").offset().top ) {
+
+			// Enable parallax effect
+			backgroundscroll = scrollwindow - $(".home-section-11").offset().top;
+			$(".home-section-11").css("backgroundPosition", "0px " + -(backgroundscroll/6) + "px");
+
+		}
+		
+
+	});
+
 });
